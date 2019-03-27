e179a478dd9dc85555a43619a90bf86151602fb5
echao2012 <47439716+echao2012@users.noreply.github.com>
2019-03-27 18:49:14 -0500

    Implemented concert-this

--- liri.js
+++ liri.js
@@ -1,2 +1,48 @@
+// Require package statements
 require("dotenv").config();
-var keys = require("./keys.js");
+const keys = require("./keys.js");
+const axios = require("axios");
+const moment = require("moment");
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
+
+const cmd = process.argv[2];
+const name = process.argv.slice(3).join(" ");
+
+switch(cmd) {
+    case "concert-this":
+        getConcert(name);
+        break;
+
+    case "spotify-this-song":
+        break;
+
+    case "movie-this":
+        break;
+
+    case "do-what-it-says":
+        break;
+    default:
+        break;
+}
+
+function getConcert(artistName) {
+    if(artistName) {
+        const queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
+        axios.get(queryUrl).then(function(response) {
+            response.data.forEach(event => {
+                console.log("Venue: " + event.venue.name);
+
+                if(event.venue.region) {
+                    console.log("Location: " + event.venue.city + ", " + event.venue.region + ", " + event.venue.country);
+                } else {
+                    console.log("Location: " + event.venue.city + ", " + event.venue.country);
+                }
+                console.log("Date: " + moment(event.datetime).format("MM/DD/YYYY"));
+                console.log();
+            });
+        });
+    } else {
+        console.log("Please provide an artist name");
+    }
+}
